687a1fbe20b9fb772885dead3c394028c32109bf
Faelxizz980 <shaolindopneu68@gmail.com>
2025-04-23 00:16:59 -0300

    teste para envio

---
+++ components/chat/chat.js
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Image, ImageBackground, KeyboardAvoidingView, Platform, TouchableOpacity } from "react-native";
+import jett from '../../img/jett.jpg';
+import cypher from '../../img/cypher.jpg';
+import giphySticker from '../../img/giphy.gif';
+import Icon from "react-native-vector-icons/Ionicons";
+
+export default function App() {
+    const [message, setMessage] = useState(""); // Estado para capturar a mensagem
+
+    return (
+        <ImageBackground source={cypher} style={styles.wallpaper}>
+            <KeyboardAvoidingView
+                style={styles.overlay}
+                behavior={Platform.OS === "ios" ? "padding" : "height"} // Ajusta o layout no iOS e Android
+            >
+                <View style={styles.container}>
+                    <View style={styles.menu}>
+                        <Icon name="arrow-back-outline" style={styles.iconVoltar} />
+                        <Image source={jett} style={styles.fotoPerfil} />
+                        <Text style={styles.nome}>Jett</Text>
+                        <Icon name="videocam-outline" style={styles.iconsMenu}></Icon>
+                        <Icon name="call-outline" style={styles.iconsMenu}></Icon>
+                    </View>
+
+                    {/* Balões de conversa */}
+                    <View style={styles.chatContainer}>
+                        {/* Balão do Cypher */}
+                        <View style={styles.balaoDestinatario}>
+                            <Text style={styles.textoBalao}>Jett, você está pronta para a missão?</Text>
+                        </View>
+
+                        {/* Balão da Jett */}
+                        <View style={styles.balaoRemetente}>
+                            <Text style={styles.textoBalao}>Sempre pronta, Cypher! Vamos detonar.</Text>
+                        </View>
+
+                        <View style={styles.balaoDestinatario}>
+                            <Text style={styles.textoBalao}>Lembre-se, eu estarei de olho em tudo.</Text>
+                        </View>
+
+                        {/* Balão da Jett */}
+                        <View style={styles.balaoRemetente}>
+                            <Text style={styles.textoBalao}>Haha, claro! Só não se perca nas suas câmeras.</Text>
+                        </View>
+
+                        {/* Balão do Cypher */}
+                        <View style={styles.balaoDestinatario}>
+                            <Text style={styles.textoBalao}>Eu nunca me perco, Jett. Boa sorte.</Text>
+                        </View>
+
+                        <View style={styles.balaoDestinatario}>
+                            <Image
+                                source={giphySticker}
+                                style={styles.sticker}
+                            />
+                        </View>
+                    </View>
+
+                    {/* Caixa de entrada flutuante */}
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Digite sua mensagem..."
+                            placeholderTextColor="#aaa"
+                            value={message}
+                            onChangeText={setMessage}
+                        />
+                        <TouchableOpacity onPress={() => alert(`Mensagem enviada: ${message}`)}>
+                            <Icon name="send-outline" style={styles.sendIcon} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </KeyboardAvoidingView>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    wallpaper: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // semi-transparent black
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        flex: 1,
+        width: '100%',
+    },
+    menu: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        marginTop: 25,
+    },
+    iconVoltar: {
+        fontSize: 24,
+        color: '#fff',
+        marginRight: 10,
+    },
+    fotoPerfil: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 10,
+    },
+    nome: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        flex: 1,
+    },
+    iconsMenu: {
+        fontSize: 24,
+        color: '#fff',
+        marginLeft: 10,
+    },
+    chatContainer: {
+        flex: 1,
+        padding: 10,
+    },
+    balaoRemetente: {
+        alignSelf: 'flex-start',
+        backgroundColor: '#bbc6ce',
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 10,
+        maxWidth: '70%',
+    },
+    balaoDestinatario: {
+       
+        alignSelf: 'flex-end',
+        backgroundColor: '#3c506e',
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 10,
+        maxWidth: '70%',
+    },
+    textoBalao: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    sticker: {
+        width: 150,
+        height: 150,
+        borderRadius: 10,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#3c506e',
+        borderTopWidth: 1,
+        borderTopColor: '#555',
+    },
+    input: {
+        flex: 1,
+        height: 40,
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        paddingHorizontal: 15,
+        fontSize: 16,
+    },
+    sendIcon: {
+        fontSize: 24,
+        color: '#fff',
+        marginLeft: 10,
+    },
+});
---
+++ components/contatos/contatos.js
@@ -0,0 +1,103 @@
+import React  from "react";
+import { StyleSheet, Text, View, TextInput,Image } from "react-native";
+import clove from  '../../img/clove.jpg';
+import jett from '../../img/jett.jpg';
+import Icon from "react-native-vector-icons/Ionicons";
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <View style ={styles.Menu} >
+                <Text style={styles.titulo}>Converse </Text>
+                <TextInput style={styles.search} placeholder="Pesquisar" />
+            </View>
+            <View style = {styles.perfil}>
+                <Image source={clove} style={styles.contactImage} />
+                <Text style={styles.nomeContato}>clove</Text>
+                <Text style={styles.conversas}> <Icon name="checkmark-done-outline" style = {styles.visu}></Icon> Você: vem cair no meu fiozinho</Text>
+            </View>
+            <View style = {styles.perfil}>
+                <Image source={jett} style={styles.contactImage} />
+                <Text style={styles.nomeContato}>Jett</Text>
+                <Text style={styles.conversas}> <Icon name="checkmark-done-outline" style = {styles.visu}></Icon> Jett: Salve careca preta</Text>
+            </View>
+        </View>
+    );
+  
+}
+
+const styles = StyleSheet.create({
+    visu:{
+       color: '#bbc6ce',
+       fontSize: 14,
+       fontWeight: 'bold',
+       fontFamily: 'sans-serif',  
+    },
+  container: {
+    flex: 1,
+    backgroundColor: '#03020e',
+  width: '100%',
+  },
+  titulo: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginLeft: 20,
+    fontFamily: 'sans-serif',  
+    marginTop: 50,
+  },
+
+  search: {
+    color: '#052e6d',
+    fontSize: 18,
+    fontWeight: 'bold',
+    backgroundColor: '#3c506e',
+    width: '90%',
+    height: 40,
+    borderRadius: 20,
+    marginTop: 30,
+    marginLeft: 20,
+    paddingLeft: 20,
+  },
+  contactImage: {
+    width: 60, // Largura da imagem
+    height: 60, // Altura da imagem
+    borderRadius: 50, // Torna a imagem circular
+    marginTop: 30, // Margem superior
+    marginLeft: 20, // Margem esquerda
+},
+    nomeContato:{
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold',
+        position: 'absolute',
+        top: 40,
+        left: 90,
+        fontFamily: 'sans-serif',  
+    },
+    nomeContato2:{
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold',
+        position: 'absolute',
+        top: 40,
+        left: 90,
+        fontFamily: 'sans-serif',  
+    },
+    conversas:{
+        color: '#bbc6ce',
+        fontSize: 14,
+        fontWeight: 'bold',
+        position: 'absolute',
+        top: 60,
+        left: 90,
+        fontFamily: 'sans-serif',  
+    },
+
+    outline:{
+        color: '#bbc6ce',
+       justifyContent: 'flex-end',
+    },
+});
+
